d0ad28c3b437359fa1ae0fc9d5df257889e65fee
Pavlo_Sernetskyi <74708976+PavloSernetskyi@users.noreply.github.com>
2025-05-25 16:17:38 -0700

    added custom themed image with shake effect

--- app/src/main/java/com/example/memorygame/MainActivity.java
+++ app/src/main/java/com/example/memorygame/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.memorygame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         Button exitBtn = findViewById(R.id.exitBtn);
 
         exitBtn.setOnClickListener(v -> finishAffinity());
+
+        ImageView image = findViewById(R.id.gameImage);
+        Animation shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        image.startAnimation(shake);
+
 
         startGameBtn.setOnClickListener(v -> showNamePrompt());
         viewScoreboardBtn.setOnClickListener(v -> {
